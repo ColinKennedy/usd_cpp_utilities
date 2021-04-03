27e681df23bc0b3ec077f14440ba569f5bf14241
Colin Kennedy <colinvfx@gmail.com>
2021-04-02 17:47:56 -0700

    Adding the first basic files

---
+++ README.md
@@ -0,0 +1,7 @@
+- Get the basic code to build
+- Build Python bindings
+- Make rez tests
+- Add CUDA support, via ephemerals
+- Optimize it to be as fast as possible
+
+- Make sure that I can include it into another Rez package
--- package.py
+++ package.py
@@ -10,3 +10,14 @@ description = "Miscellaneous, optimized functions for working with USD"
 authors = ["ColinKennedy"]
 
 private_build_requires = ["cmake-3"]
+
+# TODO : Can this be private / build requires?
+requires = ["USD-20.02.3+<21"]
+
+
+def commands():
+    import os
+
+    # TODO : Check that these values are required
+    env.LIBRARY_PATH.append(os.path.join(root, "lib"))
+    env.LD_LIBRARY_PATH.append(os.path.join(root, "lib"))
