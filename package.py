2f65645b85a96317d3a9c1fe23101faa6d76bbdd
Colin Kennedy <colinvfx@gmail.com>
2021-04-02 17:25:35 -0700

    Initial commit

---
+++ package.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+name = "usd_cpp_utilities"
+
+version = "1.0.0"
+
+description = "Miscellaneous, optimized functions for working with USD"
+
+authors = ["ColinKennedy"]
+
+private_build_requires = ["cmake-3"]
