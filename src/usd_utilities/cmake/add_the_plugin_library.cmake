d6cf7adbeb23b2b154cabf53576db3cbb13677e5
Colin Kennedy <colinvfx@gmail.com>
2021-04-04 19:37:16 -0700

    Split the CMakeLists.txt into multiple files to make it easier to read

---
+++ src/usd_utilities/cmake/add_the_plugin_library.cmake
@@ -0,0 +1,25 @@
+# Add a basic definition for this USD C++ library.
+#
+# This definition will be exported and given Python bindings. But not
+# in this file.  This file focuses just on the basic definition of the
+# plugin.
+#
+add_library(${PLUGIN_NAME}
+    SHARED
+        common.cpp
+        scale_check.cpp
+)
+
+target_include_directories(${PLUGIN_NAME}
+    PRIVATE
+        ${PYTHON_INCLUDE_PATH}
+        ${PXR_INCLUDE_DIRS}
+)
+
+target_link_libraries(${PLUGIN_NAME}
+    ${PXR_sdf_LIBRARY}
+    ${PXR_usd_LIBRARY}
+    ${PXR_usdGeom_LIBRARY}
+    ${PXR_vt_LIBRARY}
+    ${USD_BOOST_PYTHON}
+)
---
+++ src/usd_utilities/cmake/export_the_plugin.cmake
@@ -0,0 +1,38 @@
+# Export a namespace for this library so other C++ projects can use it
+#
+# Reference: https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
+#
+set(PLUGIN_TARGETS_NAME UsdUtilitiesTargets)
+set(INSTALL_CONFIGURATION_DIRECTORY lib/cmake/UsdUtilities)
+
+install(
+    TARGETS ${PLUGIN_NAME}
+    EXPORT ${PLUGIN_TARGETS_NAME}
+    LIBRARY DESTINATION lib
+    INCLUDES DESTINATION include
+)
+
+install(
+    EXPORT ${PLUGIN_TARGETS_NAME}
+    FILE ${PLUGIN_TARGETS_NAME}.cmake
+    NAMESPACE usd_utilities::
+    DESTINATION ${INSTALL_CONFIGURATION_DIRECTORY}
+)
+
+# Create a Config.cmake file so that other C++ projects can use
+# `find_package` to get the namespace that was exported in step #5
+#
+# Reference: https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right
+#
+include(CMakePackageConfigHelpers)
+
+configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/UsdUtilitiesConfig.cmake
+    ${CMAKE_CURRENT_BINARY_DIR}/UsdUtilitiesConfig.cmake
+    INSTALL_DESTINATION ${INSTALL_CONFIGURATION_DIRECTORY}
+)
+
+# Copy the generated UsdUtilitiesConfig.cmake to the installation directory.
+install(FILES
+    ${CMAKE_CURRENT_BINARY_DIR}/UsdUtilitiesConfig.cmake
+    DESTINATION ${INSTALL_CONFIGURATION_DIRECTORY}
+)
