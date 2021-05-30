86bf7a5d1f5b465cbb39f86e1f09277cf9056c18
Colin Kennedy <colinvfx@gmail.com>
2021-05-30 15:11:44 -0700

    Got CMake to build

--- src/python_bindings/wrap_itermaker.cpp
+++ src/python_bindings/wrap_itermaker.cpp
@@ -7,5 +7,11 @@
 using namespace boost::python;
 
 BOOST_PYTHON_MODULE(itermaker) {
-  def("iter_prim_specs", usd_utilities::iter_prim_specs);
+  def("iter_prim_specs", static_cast<std::vector<pxr::SdfPrimSpecHandle> (*)(
+                             pxr::SdfPrimSpecHandle const &prim_spec)>(
+                             &usd_utilities::iter_prim_specs));
+
+  // def("iter_prim_specs",
+  //     static_cast<std::vector<pxr::SdfPrimSpecHandle> (*)(
+  //         pxr::Layer const &layer)>(&usd_utilities::iter_prim_specs));
 }
--- src/usd_utilities/itermaker.cpp
+++ src/usd_utilities/itermaker.cpp
@@ -1,11 +1,17 @@
+#include <pxr/usd/sdf/layer.h>
 #include <pxr/usd/sdf/primSpec.h>
 #include <vector>
 
 namespace usd_utilities {
-std::vector<pxr::SdfPrimSpec>
-iter_prim_specs(pxr::SdfPrimSpec const &prim_spec) {
-  std::vector<pxr::SdfPrimSpec> output;
+std::vector<pxr::SdfPrimSpecHandle>
+iter_prim_specs(pxr::SdfPrimSpecHandle const &prim_spec) {
+  std::vector<pxr::SdfPrimSpecHandle> output;
 
   return output;
+}
+
+std::vector<pxr::SdfPrimSpecHandle>
+iter_prim_specs(pxr::SdfLayer const &layer) {
+  return iter_prim_specs(layer.GetPseudoRoot());
 }
 } // namespace usd_utilities
