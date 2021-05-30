5e4de56a28ad8a65da571a813ccbde022af7e49b
Colin Kennedy <colinvfx@gmail.com>
2021-05-30 15:44:18 -0700

    Getting C++ code ready for unittests

--- src/usd_utilities/itermaker.cpp
+++ src/usd_utilities/itermaker.cpp
@@ -1,12 +1,37 @@
 #include <pxr/usd/sdf/layer.h>
 #include <pxr/usd/sdf/primSpec.h>
+#include <pxr/usd/sdf/proxyTypes.h>
+#include <pxr/usd/sdf/variantSetSpec.h>
+#include <pxr/usd/sdf/variantSpec.h>
 #include <vector>
 
 namespace usd_utilities {
 std::vector<pxr::SdfPrimSpecHandle>
-iter_prim_specs(pxr::SdfPrimSpecHandle const &prim_spec) {
-  std::vector<pxr::SdfPrimSpecHandle> output {prim_spec};
+iter_prim_specs(pxr::SdfPrimSpecHandle const &root) {
+  std::vector<pxr::SdfPrimSpecHandle> output{};
 
+  auto children = root->GetNameChildren();
+  output.reserve(children.size() + 1);
+  output.push_back(root);
+
+  for (auto const &prim_spec : children) {
+    for (auto const &variant_set : prim_spec->GetVariantSets()) {
+      auto variants = variant_set.second;
+      output.reserve(variants.size());
+
+      for (auto const &data : variants) {
+        auto const variant_spec = data.second;
+
+        for (auto const &inner : iter_prim_specs(variant_spec->GetPrimSpec())) {
+          output.push_back(inner);
+        }
+      }
+    }
+
+    for (auto const &child : iter_prim_specs(prim_spec)) {
+      output.push_back(child);
+    }
+  }
 
   return output;
 }
--- tests/test_itermaker.py
+++ tests/test_itermaker.py
@@ -23,7 +23,9 @@ class IterPrimSpec(unittest.TestCase):
 
     def test_single(self):
         """Return a single child."""
-        raise ValueError()
+        layer = Sdf.Layer.CreateAnonymous(".usda")
+        Sdf.CreatePrimInLayer(layer, "/root/another")
+        raise ValueError(list(_iter_from_layer(layer)))
 
     def test_multiple(self):
         """Return all children."""
@@ -36,3 +38,82 @@ class IterPrimSpec(unittest.TestCase):
     def test_performance(self):
         """Make sure the C++ function is faster than Python."""
         raise ValueError()
+
+    def test_variants(self):
+        layer = Sdf.Layer.CreateAnonymous(".usda")
+        layer.ImportFromString(
+            """\
+            #usda 1.0
+
+            def "root"
+            {
+                variantSet "thing" = {
+                    "one" {
+                        def "variant_child"
+                        {
+                            def "variant_grand_child_1"
+                            {
+                            }
+
+                            def "variant_grand_child_2"
+                            {
+                            }
+                        }
+
+                        def "foo"
+                        {
+                        }
+                    }
+                }
+
+                def "child"
+                {
+                    def "grand_child_1"
+                    {
+                    }
+
+                    def "grand_child_2"
+                    {
+                    }
+                }
+            }
+            """
+        )
+
+        results = list(_iter_from_layer(layer))
+
+        self.assertEqual(
+            [
+                layer.GetPrimAtPath("/"),
+                layer.GetPrimAtPath("/root{thing=one}"),
+                layer.GetPrimAtPath("/root{thing=one}variant_child"),
+                layer.GetPrimAtPath("/root{thing=one}variant_child/variant_grand_child_1"),
+                layer.GetPrimAtPath("/root{thing=one}variant_child/variant_grand_child_2"),
+                layer.GetPrimAtPath("/root{thing=one}foo"),
+                layer.GetPrimAtPath("/root"),
+                layer.GetPrimAtPath("/root/child"),
+                layer.GetPrimAtPath("/root/child/grand_child_1"),
+                layer.GetPrimAtPath("/root/child/grand_child_2"),
+            ],
+            results,
+        )
+
+        raise ValueError(results)
+
+
+def _iter_prim_spec(root):
+    yield root
+
+    for prim_spec in root.nameChildren:
+        for variant_set in prim_spec.variantSets:
+            for variant_spec in variant_set.variants.values():
+                for inner in _iter_prim_spec(variant_spec.primSpec):
+                    yield inner
+
+        for child in _iter_prim_spec(prim_spec):
+            yield child
+
+
+def _iter_from_layer(layer):
+    for prim_spec in _iter_prim_spec(layer.pseudoRoot):
+        yield prim_spec
