9b594a41f52be1043d0446a12cdb8f0759bdd7b7
Colin Kennedy <colinvfx@gmail.com>
2021-05-30 14:51:51 -0700

    Fixed a linker error

--- src/usd_utilities/itermaker.cpp
+++ src/usd_utilities/itermaker.cpp
@@ -1,9 +1,11 @@
 #include <pxr/usd/sdf/primSpec.h>
 #include <vector>
 
+namespace usd_utilities {
 std::vector<pxr::SdfPrimSpec>
 iter_prim_specs(pxr::SdfPrimSpec const &prim_spec) {
   std::vector<pxr::SdfPrimSpec> output;
 
   return output;
 }
+} // namespace usd_utilities
